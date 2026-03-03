6493ca0b6b0f2274460fcf3b38700fff64ef3042
kgozman6159 <34196425+kgozman6159@users.noreply.github.com>
2026-03-03 14:01:55 -0500

    clean up code, add a few comments, get rid of unneeded imports

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-import time
-
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from astropy.io import ascii
 import altair as alt
 import astropy.units as u
 from astropy.constants import c
-from pathlib import Path
 from astropy.constants import L_sun
-# from dotenv import load_dotenv
-# import dropbox
-import os
-import shutil
 import matplotlib.colors as mcolors
 
 st.set_page_config(layout="wide",
@@ -25,25 +17,6 @@ st.set_page_config(layout="wide",
     'About':"If you have any feature requests or find any bugs, please report them on the [GitHub](https://github.com/marmicolta/AREPAS/issues) page or contact us at micoltam [at] umich [dot] edu or kgozman [at] umich [dot] edu"""
   })
 
-
-# --------- Dropbox API setup ---------- #
-# .env should be in gitignore but really not gonna make Nuria figure out APIs
-
-# load_dotenv()  # reads .env automatically. 
-# DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
-
-# if DROPBOX_TOKEN is None:
-#     raise RuntimeError(
-#         "DROPBOX_TOKEN not found. "
-#         "Create a .env file with your Dropbox access token."
-#     )
-
-# dbx = dropbox.Dropbox(DROPBOX_TOKEN)
-# dbx = dropbox.Dropbox(
-#     oauth2_refresh_token=os.environ["DROPBOX_REFRESH_TOKEN"],
-#     app_key=os.environ["DROPBOX_APP_KEY"],
-#     app_secret=os.environ["DROPBOX_APP_SECRET"],
-# )
 
 def get_flux(flux, vel, line):
     
@@ -74,33 +47,12 @@ def get_flux(flux, vel, line):
 
 
 #--------- Get data model from Github---------- #
-# CACHE_DIR = Path.home() / ".cache" / "magnetomodels"
-# CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
 # Function to get model file from dropbox
 def get_model_file(fname,spt,line):
     
-    # # Check if file exists in cache
-    # local_path = CACHE_DIR / f"{spt}.{fname}"
-    # # If not, download it from dropbox
-    # if not local_path.exists():
-    #     # print(spt)
-    #     print(f"/Profiles/{spt}profiles/{fname}")
-    #     download_from_dropbox(f"/Profiles/{spt}profiles/{fname}", local_path)
     url = f"https://raw.githubusercontent.com/marmicolta/data_magneto_models/refs/heads/main/{spt}profiles/{line}/{fname}"
     return url
-
-
-# # Function to download file from dropbox and save to local path
-# def download_from_dropbox(remote_path, local_path):
-#     print(f"Downloading {remote_path} from Dropbox...")
-
-#     md, res = dbx.files_download(remote_path)
-
-#     with open(local_path, "wb") as f:
-#         f.write(res.content)
-
-
 
 
 @st.cache_data
@@ -112,7 +64,7 @@ def load_data(file_name,spectral_type,line):
     return data
 
 
-# --------- Page Title ---------- #
+# --------- Page Title and Intro ---------- #
 st.set_page_config(layout="wide")
 
 st.title("AREPAS🫓: A Resource for Exploring Protostellar Accretion Systems")
@@ -144,6 +96,7 @@ with st.expander("How to use this app"):
 
 @st.cache_data
 def load_ids():
+    # --- loads in supplementary file with model parameters, which is used to find the correct file name for a given set of parameters --- #
     data = pd.read_csv('magnetomodels-ids.csv')
     return data
 mag_ids = load_ids()
@@ -166,9 +119,9 @@ mdots = {'M01':{'val':1.00e-7,'tmin':0,'tmax':8  },\
          }
 
 temps_list = mag_ids['Tmax'][~np.isnan(mag_ids['Tmax'])].unique()
-# print(temps_list)
+
 mdot_list = mag_ids['Mdot'][~np.isnan(mag_ids['Mdot'])].unique()
-# print(mdot_list)
+
 
 lines = {'h23':r"Hα",
          'h24':r"Hβ", 
@@ -201,24 +154,20 @@ line_centers = {'h23':656.279, \
          'mg12':280.367
         }     
 
-# temps_ = temps[mdots[md]['tmin']:mdots[md]['tmax']]
+
 
 # Create an empty dataframe on first page load, will skip on page reloads
 if 'data' not in st.session_state:
     data = pd.DataFrame({'line':[], 'Line':[], 'Mdot':[],'Tmax':[],'Rin':[], 'Width':[], 'Inclination':[], 'Abundance':[], "SpectralType":[], "Int_Flux":[], "Velocity":[], 'Fnu':[], 'Flam':[], 'Luminosity':[],'Nflux':[], 'Label':[], 'Filename':[]})
     st.session_state.data = data
 
-# --- button to clear the dataframe ---
+# --- button to clear the dataframe ---#
 def clear_data():
     print(st.session_state.data)
     st.session_state.data = pd.DataFrame({'line':[],'Line':[], 'Mdot':[],'Tmax':[],'Rin':[], 'Width':[], 'Inclination':[], 'Abundance':[], "SpectralType":[], "Int_Flux":[], "Velocity":[], 'Fnu':[], 'Flam':[], 'Luminosity':[],'Nflux':[], 'Label':[], 'Filename':[]})
     st.session_state.all_data = pd.DataFrame({'Velocity':[], 'Flam':[], 'Nflux':[], 'Label':[]})
     print(st.session_state.data)
 st.button('Clear Data', on_click=clear_data, help='Clears all selected model parameters and plotted data.')
-
-# if 'all_data' not in st.session_state:
-#     all_data = pd.DataFrame({'Velocity':[], 'Flux':[], 'Nflux':[], 'Label':[]})
-#     st.session_state.all_data = all_data
 
 
 # Function to append non-form inputs into dataframe
@@ -256,14 +205,11 @@ def add_df(user_data=False):
     if not exists:
         st.session_state.data = pd.concat([st.session_state.data, row], ignore_index=True)
 
-        # get_flux_data()
-    # st.session_state.data = pd.concat([st.session_state.data, row], ignore_index=True)
 
 
 
 # --------- Sidebar for parameter selection ---------- #
 with st.sidebar:
-    # Inputs created outside of a form (allows computing col4 for preview)
 
     st.header('Model Parameter Selection')
 
@@ -305,49 +251,37 @@ with st.sidebar:
     file_name = f'prof.{line}{abundance}.G{geo_idx:02d}.M{mdot_idx[0][0]+1:02d}.T{tmax_idx:02d}.I{int(inc)}.0'
 
     st.session_state.filename = file_name
+    
     # Load the data
     profdata = load_data(file_name,spectral_type,line)
     vel = profdata['Velocity'].data # Km/s
     fnu = profdata['Flux'].data # erg/cm^2/s/Hz at the surface of the star
-    # luminosity = profdata['Luminosity'].data
     st.session_state.vel = vel
     st.session_state.fnu = fnu
-    # st.session_state.Luminosity = luminosity
-    # print('og flux',fnu)
+
     v1,v2 = vel[0], vel[-1]
     f1,f2 = fnu[0],fnu[-1]
 
     m = (f2-f1)/(v2-v1)
     f_cont = m * (vel-v1) + f1
-    # print('cont',f_cont)
-    # print(c.to('km/s'))
-    # print(line_centers[line])
+
     wave = (vel*(u.km/u.s)/c.to('km/s')  + 1)*(line_centers[line]*u.nm).to(u.AA)
-    flb = fnu * (u.erg/u.s/u.cm**2/u.Hz) * c.to(u.AA/u.s) / ((line_centers[line]*u.nm).to(u.AA))**2
+    flb = fnu * (u.erg/u.s/u.cm**2/u.Hz) * c.to(u.AA/u.s) / ((line_centers[line]*u.nm).to(u.AA))**2 # convert from Fnu to Flam
     flb = flb.to(u.erg/u.s/u.cm**2/u.AA)
-    #  "Flam":[st.session_state.flam],
+
     st.session_state.Flam = flb.value
-    # print('wave', wave)
-   # print(wave)
-    # print(flb.to(u.erg/u.s/u.cm**2/u.AA))
+
 
     # get radius of star based on spectral type from file
     stellar_params = pd.read_csv('https://raw.githubusercontent.com/marmicolta/database_magneto_models/refs/heads/main/model_stellar_parameters.csv')
-    # radius = float(stellar_params.loc[stellar_params['SpT'] == spectral_type]['R'])
     radius = float(stellar_params.iloc[np.where(stellar_params['SpT'] == spectral_type)[0][0]]['R'])*u.Rsun
-    # print('radius', radius, radius.unit)
     radius = radius.to(u.cm)
-    # print('radius', radius, radius.unit)
-    # luminosity = 4*np.pi*radius**2*(fnu-f_cont) # should be flux above the continuum!
+
   
     flb_res = ( (fnu-f_cont) * (u.erg/u.s/u.cm**2/u.Hz) * c.to(u.AA/u.s) / (wave)**2).to(u.erg/u.s/u.cm**2/u.AA) 
-    # print('res',flb_res)
+
     luminosity = 4*np.pi*radius**2* (flb_res)
-    # print('lum', luminosity/L_sun.cgs)
 
-    # print('radius', radius)
-
-    # print("MODEL", luminosity, L_sun.cgs, luminosity/L_sun.cgs)
     st.session_state.Luminosity = luminosity/L_sun.cgs # convert to Lsun
     st.session_state.Nflux = fnu/f_cont
 
@@ -359,7 +293,7 @@ with st.sidebar:
 
     label = label.replace('.Ca',' Ca Solar').replace('_0p5',' 50% Solar').replace('_0p1',' 10% Solar').replace('_0p01',' 1% Solar')
     st.session_state.label = label
-    # print(st.session_state.Int_Flux)
+
     st.button('Submit', on_click=add_df)
 
 # -------let user upload their own models ------- #
@@ -383,23 +317,15 @@ def add_user_file():
         f1,f2 = flb[0],flb[-1]
 
         m = (f2-f1)/(v2-v1)
-        f_cont = m * (vel-v1) + f1
-        # print(dist**2 * 4 * np.pi * (user_data['Flux']-f_cont))
-        # print(L_sun.cgs)
+        f_cont = m * (vel-v1) + f1 
+
         st.session_state.Luminosity = (dist**2 * 4 * np.pi * (user_data['Flux']-f_cont))/L_sun.cgs #should be flux above the continuum!
         st.session_state.Nflux = flb/f_cont
 
-        # fig = plt.figure()
-        # plt.plot(vel, flb)
-        # plt.plot(vel, f_cont)
-        # plt.savefig("/Users/kgozman/Downloads/test.png")
-
-        # st.session_state.Int_Flux = np.nan
-        # add_df()
 
         st.session_state.data = pd.concat([st.session_state.data, pd.DataFrame({'line':[uploaded_file.name], 'Line':[uploaded_file.name], 'Mdot':[np.nan],'Tmax':[np.nan],'Rin':[None], 'Width':[None], 'Inclination':[np.nan], 'Abundance':[""], "SpectralType":[np.nan], "Int_Flux":[np.nan], "Velocity":[user_data['Velocity']], 'Flam':[user_data['Flux']], 'Luminosity':[st.session_state.Luminosity], 'Nflux':[st.session_state.Nflux], 'Label':[uploaded_file.name], 'Filename':[uploaded_file.name]})], ignore_index=True)
 
-# check that file has first 2 columns that are numerical
+# check that file has first 3 columns that are numerical
 if uploaded_file is not None:
     print(uploaded_file.name)
     try:
@@ -414,25 +340,23 @@ if uploaded_file is not None:
 
 
 
-# Show current data
-# df = st.dataframe(st.session_state.data, width='content', on_select='rerun', selection_mode='multi-row')
+# ---- create a custom colormap for the dataframe gradients ---- #
 if (st.context.theme.type == "dark"):
+    
     colors= ["#000000FF","#421755FF"]
 else:
     colors= ["#FFFFFFFF","#E3CDEDFF"]
 
-# 2. Create the colormap object
-# The colors are automatically spaced evenly from 0 to 1
+
 custom_cmap = mcolors.LinearSegmentedColormap.from_list("my_gradient", colors, N=20)
 custom_cmap_r = mcolors.LinearSegmentedColormap.from_list("my_gradient_r", colors[::-1], N=20)
 
 # make gmap an integer mapping of the line names from 0-10
 line_map = {line: i for i, line in enumerate(st.session_state.data['Line'].unique())}
 abundance_map = {abundance: i for i, abundance in enumerate(st.session_state.data['Abundance'].unique()) if pd.notna(abundance)}
-# print('abund', st.session_state.data['Abundance'].unique())
-# abundance_map[np.nan] = 0  # Map NaN values to 0
-# print(abundance_map)
 spectraltype_map = {spt: i for i, spt in enumerate(st.session_state.data['SpectralType'].unique())}
+
+
 styled_df = st.session_state.data.style.background_gradient(cmap=custom_cmap_r, axis=0, subset=['Line'], gmap=st.session_state.data['Line'].map(line_map))#, vmin=mdot_list.min(), vmax= mdot_list.max())
 styled_df = styled_df.background_gradient(cmap=custom_cmap, axis=0, subset=['Mdot'])#, vmin=mag_ids['Mdot'].min(), vmax=mag_ids['Mdot'].max())
 styled_df = styled_df.background_gradient(cmap=custom_cmap, axis=0, subset=['Tmax'])#, vmin=temps_list.min(), vmax=temps_list.max())
@@ -444,111 +368,42 @@ styled_df = styled_df.background_gradient(cmap=custom_cmap, axis=0, subset=['Spe
 
 # if user-added file, don't apply gradient to parameter columns
 styled_df = styled_df.apply(lambda x: ['background-color: transparent' if x['line'] not in lines.keys() else '' for i in x], axis=1)
+
+# show current dataframe with the option to select rows for plotting
 event = st.dataframe(styled_df, width='stretch', on_select='rerun', selection_mode='multi-row', 
                      column_config={"Label": None, "Filename": None, "Velocity": None, "Flam": None, "Fnu": None, "Nflux": None, 'Luminosity': None,
                                     "line": None, "Int_Flux": st.column_config.NumberColumn("Integrated Flux (erg/s/cm^2)",format="%.2e"), "SpectralType": st.column_config.TextColumn("Spectral Type") ,
                                     "Mdot": st.column_config.NumberColumn("Mdot (Msun/yr)", format="%.2f"), "Tmax": st.column_config.NumberColumn("Tmax (K)", format="%.0f"), "Rin": st.column_config.NumberColumn("Rin (R*)", format="%.1f"), "Width": st.column_config.NumberColumn("Width (R*)", format="%.1f"), "Inclination": st.column_config.NumberColumn("Inclination (deg)", format="%.0f"), "Abundance": st.column_config.TextColumn("Abundance")})
-# st.session_state.data.style.format({'Int_Flux': "{:.2E}"})
 
-# --------- find the indices for file naming ---------- #
-# Store each profile's data in a list for overplotting
-# if 'profiles' not in st.session_state:
-#     st.session_state.profiles = []
 
-# get list of filenames from pandas dataframe
-# print(st.session_state.data.keys())
-# all_data = pd.DataFrame()
-# st.dataframe(st.session_state.data)
-# st.dataframe(st.session_state.all_data)
-# normalize_flux = st.checkbox('Normalize Flux', value=False, help='If checked, the flux will be normalized so the continuum level has a value of 1.')
+# ---- options for plotting flux, normed flux, or luminosity and to hide legend ---- #
 yaxis = st.pills('What do you want to plot on the y-axis?', ['Flux', "Normalized Flux", "Luminosity"], key='yaxis', help='''Select whether to plot flux, flux normalized to the continuum, or luminosity on the y-axis. \n If comparing against observations, it is recommended to plot either luminosity or normalized flux.''', default='Flux')
 hide_legend = st.checkbox('Hide Legend', value=False, help='If checked, the legend will be hidden from the plot.')
-# print(yaxis)
 
 
+# get only the selected rows from the dataframe and combine the data for those rows into one dataframe for plotting
 def get_flux_data():
     rows = event.selection.rows
     filtered_df = st.session_state.data.iloc[rows]
-    # st.dataframe(filtered_df)
-    # 'Velocity':[], 'Flam':[], 'Nflux':[], 'Label':[]
+
     st.session_state.all_data = pd.DataFrame({'Velocity':[], 'Flam':[], 'Luminosity':[], 'Nflux':[], 'Label':[]})
 
     # for row in st.session_state.data.itertuples():
     for row in filtered_df.itertuples():
-        # print(row)
-        # line = row.line
-        # Mdot = row.Mdot
-        # Tmax = row.Tmax
-        # Rin = row.Rin
-        # width = row.Width
-        # inc = row.Inclination
-        # print(row)
-        # abundance = row.Abundance
-        # if abundance==None:
-        #     abundance = ""
-        # spectype = row._8
-        # mdot_idx = np.where(mdot_list  == row.Mdot)
-        # tmax_idx = np.where(temps_list == row.Tmax)[0][0]+1
-
-        # # print(Rin)
-        # # print(width)
-        # #We wanr the index where the mags_id row has the combination of Ri and width
-        # geo_idx = mag_ids[ (mag_ids['Rin'] == row.Rin) & (mag_ids['Width'] == row.Width)]['ID'].values[0]
-        # # print(geo_idx)
-
-
-        # file_name = f'prof.{row.line}{abundance}.G{geo_idx:02d}.M{mdot_idx[0][0]+1:02d}.T{tmax_idx:02d}.I{int(row.Inclination)}.0'
-        # # print(file_name)
-        # # files_to_plot.append((file_name, spectral_type))
-
-        # # Load the data
-        # profdata = load_data(file_name,spectype,row.line)
-        # vel = profdata['Velocity'].data
-        # fnu = profdata['Flux'].data
-        # v1,v2 = vel[0], vel[-1]
-        # f1,f2 = fnu[0],fnu[-1]
-
-        # m = (f2-f1)/(v2-v1)
-        # f_cont = m * (vel-v1) + f1
-
-        # Nflux = fnu/f_cont
-
-        # Int_Flux = get_flux(fnu, vel, row.line)
-        # print(row)
-        # profdata = load_data(row.Filename,row.SpectralType,row.line)
+       
         vel = row.Velocity
         flb = row.Flam   
         luminosity = row.Luminosity
         pandas_data = pd.DataFrame({'Velocity':vel, 'Flam':flb, 'Luminosity':luminosity})
-        # print(profdata)
-        # pandas_data = profdata.to_pandas()
+
         pandas_data['Nflux'] = row.Nflux
         pandas_data['Int_Flux'] = row.Int_Flux
         pandas_data['Label'] = row.Label
-        # print(normalize_flux)
-        # if normalize_flux:
-            # data['Flux'] = data['Flux']/np.max(data['Flux'])
-        # pandas_data['NFlux'] = Nflux
-        #     flux_label = 'Normalized Fν'
-        # else:
-        #     flux_label = r'Fν (erg/s/cm²/Hz)'
-
-
-
-        # profile_df = pandas_data[['Velocity', 'Flux']].copy()
-        # pandas_data['Label'] = f"{lines[row.line]}, Mdot={row.Mdot:.2e}, Tmax={row.Tmax}, Rin={row.Rin}, Width={row.Width}, Inc={row.Inclination}, Spt={spectype}, Abund={abundance}"
-        # pandas_data['Label'] = f"{lines[row.line]}|{row.Mdot:.2f}|{row.Tmax:.0f}|{row.Rin}|{row.Width}|{row.Inclination:.0f}|{row.SpectralType}|{row.Abundance}|{row.Int_Flux:.2e}"
-
-        # pandas_data['Label'] = pandas_data['Label'].str.replace('.Ca',' Ca Solar').str.replace('_0p5',' 50% Solar').str.replace('_0p1',' 10% Solar').str.replace('_0p01',' 1% Solar')
-        # st.session_state.data = pd.concat([st.session_state.data, row], ignore_index=True)
-
+       
         st.session_state.all_data = pd.concat([st.session_state.all_data, pandas_data], ignore_index=True)
-        # print(profile_df)
-        # st.session_state.profiles.append(profile_df)
+       
 
-# st.dataframe(st.session_state.all_data)
 get_flux_data()
-# st.session_state.data.int_flux = int_fluxes
 
 if yaxis == 'Normalized Flux':
     flux_label = 'Normalized Fλ'
@@ -563,18 +418,14 @@ else:
     flux_param = 'Flam'
     flux_axis = alt.Axis(format=".1e")
 
-# print(st.session_state.all_data)
 
-# print(st.session_state.profiles)
 # --------- plotting the models ---------- #
 
 if not st.session_state.all_data.empty:
-    # st.dataframe(st.session_state.all_data, width='stretch')
 
     selection = alt.selection_point(fields=['Label'], bind='legend')
     selection_zoom = alt.selection_interval(bind='scales', empty='all')
     if hide_legend:
-        # selection = alt.selection_point(fields=['Label'], bind=None)
         legend=None
     else:
         legend=alt.Legend(title="Models", labelLimit=400, direction="vertical", orient="right", columns=1)
@@ -609,14 +460,7 @@ if not st.session_state.all_data.empty:
     # zoom_pan = alt.selection_interval(bind='scales')
     st.altair_chart(chart, width='stretch', )
 
-# --------- let user download data ---------- #
 
-# st.download_button(
-#     label="Download data as CSV",
-#     data=pandas_data.to_csv(index=False).encode('utf-8'),
-#     file_name=f'{file_name}.csv',
-#     mime='text/csv',
-# )
 
 # --------- footer ---------- #
 
@@ -636,11 +480,3 @@ with st.container():
     st.markdown("- [Thanathibodee et al. (2020)](https://ui.adsabs.harvard.edu/abs/2020ApJ...892...81T/abstract)")
     st.markdown("- [Thanathibodee et al. (2019)](https://ui.adsabs.harvard.edu/abs/2019ApJ...884...86T/abstract)")
 
-
-
-
-# # --------- Clear cache button ---------- #
-# st.sidebar.header("Clear Cache")
-# if st.sidebar.button("Clear Cached Data", help="Clears all cached data files from your local machine."):
-    
-#     shutil.rmtree(CACHE_DIR)  # deletes the folder and all contents
